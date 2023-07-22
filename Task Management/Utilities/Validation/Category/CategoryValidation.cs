b9b3b7806b5ad114ce8522757a3d93b9e704125e
trianisashafiera <triannisa.shafiera@gmail.com>
2023-07-22 23:00:23 +0700

    update

---
+++ Task Management/Utilities/Validation/Category/CategoryValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Task_Management.DTOs.CategoryDto;
+
+namespace Task_Management.Utilities.Validation.Category
+{
+    public class CategoryValidation : AbstractValidator<CategoryDto>
+    {
+        public CategoryValidation() 
+        {
+            RuleFor(p => p.Name)
+         .NotEmpty();
+        }
+
+    }
+}
