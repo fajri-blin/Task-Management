b27e5f605cc3d6a3f2515658ae0c0d4e036c7e82
ihsan759 <mihsandf759@gmail.com>
2023-07-24 14:22:53 +0700

    Update Validation

--- Task Management/DTOs/AccountDto/RegisterDto.cs
+++ Task Management/DTOs/AccountDto/RegisterDto.cs
@@ -1,22 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Task_Management.Utilities.Enum;
+﻿using Task_Management.Utilities.Enum;
 
 namespace Task_Management.DTOs.AccountDto;
 
 public class RegisterDto
 {
-    [Required]
     public string Username { get; set; }
-    [Required]
-    [EmailAddress]
     public string Email { get; set; }
-    [Required]
     public string Name { get; set; }
-    [Required]
-    //[PasswordPolicy]
     public string Password { get; set; }
-    [Required]
     public string? ConfirmPassword { get; set; }
-    public string Role { get; set; }
-    public string? ImageProfile { get;set; }
+    public RoleLevel Role { get; set; }
+    public string? ImageProfile { get; set; }
 }
