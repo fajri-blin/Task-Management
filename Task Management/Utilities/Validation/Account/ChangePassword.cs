b68a4593b92ea9ee24ece0bd9c6a2570f5dadd9f
ihsan759 <mihsandf759@gmail.com>
2023-07-24 21:43:41 +0700

    add validation change password

---
+++ Task Management/Utilities/Validation/Account/ChangePassword.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Task_Management.Dtos.AccountDto;
+
+namespace Task_Management.Utilities.Validation.Account
+{
+    public class ChangePassword : AbstractValidator<ChangePasswordDto>
+    {
+        public ChangePassword()
+        {
+
+            RuleFor(p => p.NewPassword)
+          .NotEmpty().WithMessage("Password is required.")
+          .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+          .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+          .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+          .Matches("[0-9]").WithMessage("Password must contain at least one digit.")
+          .Matches("[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character.");
+
+            RuleFor(p => p.ConfirmPassword)
+          .NotEmpty()
+          .Equal(model => model.NewPassword).WithMessage("Password and Confirm Password do not match.");
+        }
+    }
+}
