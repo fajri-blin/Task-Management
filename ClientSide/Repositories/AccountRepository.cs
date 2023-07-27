7c5ec0447b676ebe9ef9ae491137f4365db27fc6
trianisashafiera <triannisa.shafiera@gmail.com>
2023-07-28 06:22:22 +0700

    Add update profile

--- ClientSide/Repositories/AccountRepository.cs
+++ ClientSide/Repositories/AccountRepository.cs
@@ -19,6 +19,33 @@ public class AccountRepository : GeneralRepository<AccountVM>, IAccountRepositor
         };
         this._request = request;
     }
+    public async Task<ResponseHandlers<UpdateVM>> Update(UpdateVM updateVM)
+    {
+        ResponseHandlers<UpdateVM> entityVM = null;
+        StringContent content = new StringContent(JsonConvert.SerializeObject(updateVM), Encoding.UTF8, "application/json");
+        using (var response = _httpClient.PutAsync(_request + "Update", content).Result)
+        {
+            string apiResponse = await response.Content.ReadAsStringAsync();
+            entityVM = JsonConvert.DeserializeObject<ResponseHandlers<UpdateVM>>(apiResponse);
+        }
+        return entityVM;
+    }
+    public async Task<UpdateVM> Get(Guid guid)
+    {
+        using (var response = await _httpClient.GetAsync($"{_request}{guid}"))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                string apiResponse = await response.Content.ReadAsStringAsync();
+                UpdateVM updateVM = JsonConvert.DeserializeObject<UpdateVM>(apiResponse);
+                return updateVM;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
 
     public async Task<ResponseHandlers<ForgotPasswordVM>> ForgotPassword(ForgotPasswordVM forgotPasswordVM)
     {        
