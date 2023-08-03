103c927215278ae91fc6d6085a7995cad7054b7d
fajri-blin <fajriindonesia1945@gmail.com>
2023-08-03 14:39:58 +0700

    GetForStaff is Complete

---
+++ Task Management/DTOs/ProgressDto/GetProgressDto.cs
@@ -0,0 +1,15 @@
+﻿using Task_Management.Utilities.Enum;
+
+namespace Task_Management.DTOs.ProgressDto
+{
+    public class GetProgressDto
+    {
+        public Guid Guid { get; set; }
+        public Guid? AssignmentGuid { get; set; }
+        public string Description { get; set; }
+        public StatusEnum Status { get; set; }
+        public string? Additional { get; set; }
+        public bool CheckMark { get; set; }
+        public string? MessageManager { get; set; }
+    }
+}
